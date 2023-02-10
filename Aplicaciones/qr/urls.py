410a8b5ac9f7dddeb43dc2b430bc15c1df33242f
Your Name <you@example.com>
2023-02-09 19:42:33 -0500

    v2_1

--- Aplicaciones/qr/urls.py
+++ Aplicaciones/qr/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home)
+    path('', views.home),
+    path('registrar/', views.registrar),
+    path('editar/<name>', views.editar),
+    path('edicion/', views.edicion),
+    path('eliminar/<cedula>', views.eliminar)
 ]
--- Aplicaciones/qr/views.py
+++ Aplicaciones/qr/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import user
 
 # Create your views here.
@@ -6,3 +6,54 @@ from .models import user
 def home(request):
     usuarios = user.objects.all()
     return render(request, "gestion.html", {"usuario": usuarios})
+
+def registrar(request):
+    cedula = request.POST['txtcedula']
+    name = request.POST['txtname']
+    email = request.POST['txtemail']
+
+    sex = request.POST['txtsex']
+
+    age = request.POST['txtage']
+    country = request.POST['txtcountry']
+    city = request.POST['txtcity']
+    address = request.POST['txtadd']
+    phone = request.POST['txtphone']
+
+    usuario = user.objects.create( 
+        cedula=cedula, name=name, email=email, sex=sex, age=age, country=country, city=city, address=address, phone=phone )
+    return redirect('/')
+
+def editar(request,name):
+    usuario = user.objects.get(name=name)
+    return render(request, "editar.html", {"usuario": usuario})
+
+def edicion(request):
+    cedula = request.POST['txtcedula']
+    name = request.POST['txtname']
+    email = request.POST['txtemail']
+
+    sex = request.POST['txtsex']
+
+    age = request.POST['txtage']
+    country = request.POST['txtcountry']
+    city = request.POST['txtcity']
+    address = request.POST['txtadd']
+    phone = request.POST['txtphone']
+
+    nuevo = user.objects.get(cedula=cedula)
+    nuevo.name = name
+    nuevo.email = email
+    nuevo.sex = sex
+    nuevo.age = age
+    nuevo.country = country
+    nuevo.city = city
+    nuevo.address = address
+    nuevo.phone = phone
+    nuevo.save()
+    return redirect('/')
+
+def eliminar(request,cedula):
+    usuario = user.objects.get(cedula=cedula)
+    usuario.delete()
+    return redirect('/')
